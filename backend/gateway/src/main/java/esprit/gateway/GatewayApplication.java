e81c7043110478414e50dbb40e1cc5a91caa07ca
lamisbenhassine <lamis.benhassine@esprit.tn>
2026-02-15 18:24:11 +0000

    first commit

--- backend/gateway/src/main/java/esprit/gateway/GatewayApplication.java
+++ backend/gateway/src/main/java/esprit/gateway/GatewayApplication.java
@@ -14,10 +14,17 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-    @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
-        return builder.routes() .route("evaluation",r->r.path("/evaluation/**")
-                        .uri("http://localhost:8020/") ).build();
 
-         }
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+
+                .route("evaluation", r -> r.path("/evaluation/**")
+                        .uri("http://localhost:8020"))
+
+                .route("user", r -> r.path("/user/**")
+                        .uri("http://localhost:8010"))
+
+                .build();
+    }
 }
